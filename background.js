2e7ae711a72aabb76415530f5131e4aa64b3b026
Royserg <staniojakub@gmail.com>
2018-07-30 13:57:28 +0300

    Add background script with timer + notification
    Background script which runs all the time has running timer which fires
    notifications. Interval is taken from the popup page set by the user.
    Sent from popup to the background script.

---
+++ background.js
@@ -0,0 +1,34 @@
+var timerID;
+const timerInterval = 1000 * 60 // 1 minute in miliseconds
+
+function runTimer(time) {
+  let intervalTime = timerInterval * time;
+  // Notification Options
+  const options = {
+    type: "image",
+    title: "Straighten Up Your Back",
+    message: "Fix you posture!",
+    iconUrl: "assets/icon_128.png",
+    imageUrl: "assets/notification_img.png"
+  }
+
+  timerID = setInterval(function showNotification() {
+    chrome.notifications.create(options);
+  }, intervalTime);
+
+}
+
+chrome.runtime.onMessage.addListener(gotMessage);
+
+function gotMessage(message, sender, sendResponse) {
+  console.log(message.txt, message.time);
+  if (message.txt === 'start') {
+    // start notification interval
+    runTimer(message.time);
+  }
+
+  if (message.txt === 'stop') {
+    clearInterval(timerID);
+  }
+}
+
