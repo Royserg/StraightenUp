cb409c956f0e734c6ccec5481a03bdc71185aa1e
Royserg <staniojakub@gmail.com>
2018-07-30 13:59:51 +0300

    Setup notification interval + send it to back js
    Use clicking on popup has possibility to set interval of displaying
    notifications. This info is send to background script and fires there.
    
    Also it saves timer interval into local storage.
    Saved interval info = running timer.
    No info = timer stopped.
    
    When extension is opened it restarts whole popup page, it doesn't hold
    any info, that's why implemented saving info into storage.
    
    So Save/Stop button will be properly displayed when interval is/isn't
    in the storage

--- main.js
+++ main.js
@@ -3,34 +3,69 @@ const decreaseBtn = document.querySelector('.timer__down');
 const counter = document.querySelector('.timer__count');
 const startBtn = document.querySelector('#timerStart');
 
-increaseBtn.addEventListener('click', () => updateCounter(15), false);
-decreaseBtn.addEventListener('click', () => updateCounter(-15), false);
+// Extension icon is clicked - lookup if timer running
+chrome.storage.sync.get(['timer'], (result) => {
+  if (result.timer) {
+    // Change button to Stop
+    startBtn.innerHTML = 'Stop';
+    startBtn.classList.remove('button--green');
+    startBtn.classList.add('button--red');
+    startBtn.onclick = stopTimer;
+
+    counter.innerHTML = result.timer;
+
+    increaseBtn.style.display = 'none';
+    decreaseBtn.style.display = 'none';
+  }
+});
+
+
+increaseBtn.addEventListener('click', () => updateCounter(5), false);
+decreaseBtn.addEventListener('click', () => updateCounter(-5), false);
+startBtn.onclick = () => startTimer(counter.innerHTML);
 
 function updateCounter(value) {
   let currentCounter = Number(counter.innerHTML);
-  if (value < 0 && currentCounter === 15) {
+  if (value < 0 && currentCounter === 5) {
     // terminate function when trying to decrease below 0
     return
   }
   counter.innerHTML = (currentCounter + value);
 }
 
-
-startBtn.onclick = startTimer;
-
-
-function startTimer() {
-  // Notification Options
-  const options = {
-    type: "image",
-    title: "Straighten Up Your Back",
-    message: "Fix you posture!",
-    iconUrl: "assets/icon_128.png",
-    imageUrl: "assets/notification_img.png"
+function startTimer(time) {
+  // send time and start timer in the background;
+  let msg = {
+    txt: 'start',
+    time: Number(time)
   }
+  chrome.runtime.sendMessage(msg);
 
-  chrome.notifications.create(options);
+  chrome.storage.sync.set({timer: Number(time)}, () => {
+    console.log('Set value in the storage')
+  });
+
+  window.close();
 }
 
+function stopTimer() {
+  // Change button back and add arrows and txt
+  startBtn.innerHTML = 'Start';
+  startBtn.classList.remove('button--red');
+  startBtn.classList.add('button--green');
+  increaseBtn.style.display = 'block';
+  decreaseBtn.style.display = 'block';
 
+  // send msg to background script
+  let msg = {
+    txt: 'stop',
+  }
+  chrome.runtime.sendMessage(msg);
 
+  // remove timer value from storage
+  chrome.storage.sync.set({timer: ""}, () => {
+    console.log('Remove value of timer')
+  });
+
+  startBtn.onclick = () => startTimer(counter.innerHTML);
+}
